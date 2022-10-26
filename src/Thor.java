2b2694df1d8543a70cbbee3bffedff98e3f19c91
Rakhim9830 <106856795+Rakhim9830@users.noreply.github.com>
2022-10-26 18:33:18 +0600

    Initial commit

---
+++ src/Thor.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Thor  extends Hero{
+    boolean thorStan;
+    public Thor(int health, int damage,  String name) {
+        super(health, damage, SuperAbility.STAN, name);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        thorStan = RPG_Game.random.nextBoolean();
+        if (thorStan){
+            boss.setDamage(0);
+            System.out.println("Boss оглушен на 1 раунд");
+        }
+        else {
+            boss.setDamage(50);
+        }
+    }
+}
